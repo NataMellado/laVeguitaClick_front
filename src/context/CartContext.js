7e0dab6e7393e59a21289b174e022f58cdbefc04
NataMellado <natalia.cmc92gmail.com>
2024-11-03 12:42:03 -0300

    feat: add localStorage persistence to CartContext

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,11 +1,24 @@
-import React, { createContext, useContext, useState } from "react";
+"use client";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 const CartContext = createContext();
 
 export const useCart = () => useContext(CartContext);
 
 export const CartProvider = ({ children }) => {
+
   const [cart, setCart] = useState([]);
+
+    // Carga el carrito desde localStorage al cargar la página
+    useEffect(() => {
+      const savedCart = window.localStorage.getItem("cart");
+      setCart(savedCart ? JSON.parse(savedCart) : []);
+    }, []);
+
+    // Guarda el carrito en localStorage cada vez que cambia
+    useEffect(() => {
+      window.localStorage.setItem("cart", JSON.stringify(cart));
+    }, [cart]);
 
   const addToCart = (product) => {
     setCart((prevCart) => {
